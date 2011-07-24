64e84f23c316a47e71ef81554cd6f1969801e6dc
carl.bunting <carl.bunting@localhost>
2011-07-24 16:23:25 +0100

    Allow the WIFI scanner to be run as a standalone activity

--- src/bunting/carl/deviceinfowidget/WifiScanner.java
+++ src/bunting/carl/deviceinfowidget/WifiScanner.java
@@ -25,14 +25,34 @@ public class WifiScanner extends ListActivity {
 		if (scanResults != null) {
 			for (ScanResult scanResult: scanResults) {
 				Map<String, String> map = new HashMap<String, String>();
-				map.put("SSID", scanResult.SSID + ": " + scanResult.frequency + " " + scanResult.capabilities);
+				map.put("SSID", scanResult.SSID + ": " + WifiScanner.getChannelFromFrequency(scanResult.frequency) + " " + scanResult.capabilities);
 				accessPoints.add(map);
 			}
 		}
 
 		ListAdapter adapter = new SimpleAdapter(this, accessPoints, R.layout.wifi_scanner, new String[] {"SSID"}, new int[] {R.id.text1}); 
-		
-		setListAdapter(adapter);
+		super.setListAdapter(adapter);
+	}
+	
+	private static int getChannelFromFrequency(int frequency) {
+		switch (frequency) {
+		case 2412: return 1;
+		case 2417: return 2;
+		case 2422: return 3;
+		case 2427: return 4;
+		case 2432: return 5;
+		case 2437: return 6;
+		case 2442: return 7;
+		case 2447: return 8;
+		case 2452: return 9;
+		case 2457: return 10;
+		case 2462: return 11;
+		case 2467: return 12;
+		case 2472: return 13;
+		case 2484: return 14;
+		default:
+			return frequency;
+		}
 	}
 	
 }
